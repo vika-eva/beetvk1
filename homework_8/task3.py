2a21e70dbc6b4e2253adddf964c42eed9ba23f1f
vika-eva <50364679+vika-eva@users.noreply.github.com>
2024-10-30 18:10:44 +0200

    Update task3.py

--- homework_8/task3.py
+++ homework_8/task3.py
@@ -1,12 +1,33 @@
-def make_operation(operators, *nums:int):
+import math
+from statistics import multimode
+from operator import sub, truediv
+from functools import reduce
+
+def make_operation(operators:str, *nums:int):
     if operators == "+":
-        return nums[0] + nums[1] + nums[2]
+        return sum(nums)
     if operators == "-":
-        return nums[0] - nums[1] - nums[2] - nums[3]
+        return reduce(sub, nums)
     if operators == "*":
-        return nums[0] * nums[1]
+        return reduce((lambda x, y: x * y), nums)
     else:
         return "error"
+
 print(make_operation("+", 7, 7, 2))
 print(make_operation("-", 5, 5, -10, -20))
 print(make_operation("*", 7, 6))
+
+#def make_operation(operators, *nums:int):
+#    if operators == "+":
+#        return nums[0] + nums[1] + nums[2]
+#    if operators == "-":
+#        return nums[0] - nums[1] - nums[2] - nums[3]
+#    if operators == "*":
+#        return nums[0] * nums[1]
+#    else:
+#        return "error"
+#print(make_operation("+", 7, 7, 2))
+#print(make_operation("-", 5, 5, -10, -20))
+#print(make_operation("*", 7, 6))
+
+
